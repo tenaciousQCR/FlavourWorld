4ec381991564d2ab45c0404992d90fa6fceb09fe
ZOrvis <a.j.orvis@rgu.ac.uk>
2018-03-01 11:09:45 +0000

    Added animation to menu with JQuery. Extra functionality not enabled.

---
+++ scripting.js
@@ -0,0 +1,20 @@
+$(function(){
+    $("#sidebar").animate({width: "toggle"}, 1);
+});
+
+function menu_toggle(){
+
+    $("#sidebar").animate({width: "toggle", height: "toggle"}, 200);
+/*
+    if($("#mapContainer").hasClass("grid-70")){
+      $("#mapContainer").removeClass("mapContainer grid-70 mobile-grid-100");
+      $("#mapContainer").addClass("mapContainer grid-100 mobile-grid-100");
+    }
+    else if($("#mapContainer").hasClass("mapContainer grid-100 mobile-grid-100")){
+      $("#mapContainer").addClass("mapContainer grid-70 mobile-grid-100");
+      $("#mapContainer").removeClass("mapContainer grid-100 mobile-grid-100");
+    }
+*/
+
+
+};
