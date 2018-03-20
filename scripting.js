446f2d18a586030ff636b57bad3c2deeec1881dd
Davide0295 <davide.rendina@gmail.com>
2018-03-20 14:49:54 +0000

    made the sidebar always visible

--- scripting.js
+++ scripting.js
@@ -1,10 +1,11 @@
-$(function(){
-    $("#sidebar").animate({width: "toggle"}, 1);
-});
+// $(function(){
+//     $("#sidebar").animate({width: "toggle"}, 1);
+// });
+//
+// function menu_toggle(){
+//
+//     $("#sidebar").animate({width: "toggle", height: "toggle"}, 200);
 
-function menu_toggle(){
-
-    $("#sidebar").animate({width: "toggle", height: "toggle"}, 200);
 /*
     if($("#mapContainer").hasClass("grid-70")){
       $("#mapContainer").removeClass("mapContainer grid-70 mobile-grid-100");
