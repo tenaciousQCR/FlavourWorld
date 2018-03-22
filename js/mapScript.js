50c53a05d2d46da6f4a90ed59fd443dda8ce579e
ZOrvis <a.j.orvis@rgu.ac.uk>
2018-03-22 16:57:34 +0000

    This will break everything

---
+++ js/mapScript.js
@@ -0,0 +1,81 @@
+//set the map and initial coordinates
+var geojson;
+var popup = L.popup();
+var mymap;
+
+function style(feature) {
+    return {
+        fillColor: getColour(feature.properties.continent),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '7',
+        fillOpacity: 0.7
+    };
+}
+
+function getColour(continent) {
+    return continent == "North America" ? '#ff0000' :
+           continent == "South America"  ? '#179a13' :
+           continent == "Asia"  ? '#ffce01' :
+           continent == "Europe"  ? '#3e76ec' :
+           continent == "Africa"   ? '#E59400' :
+           continent == "Oceania"   ? '#800080' :
+                      '#808080';
+}
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+}
+
+function popupFeature(e){
+    popup
+        .setLatLng(e.latlng)
+        .setContent('<!doctype html><html><head><meta charset="utf-8"><link rel="stylesheet" href="../css/popup.css"></head><body id=\'popupBody\'><h1>Recipes</h1><div id=\'popupDiv\'><ol id= \'popupList\'><li><link>Item1</link></li><li><link>Item2</link></li><li><link>Item3</link></li></ol></div></body></html>')
+        .openOn(mymap);
+}
+
+
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: popupFeature,
+    });
+}
+
+
+
+$.getJSON('../js/custom.geo.json',function(data){
+            mymap = L.map('mapid').setView([32,-35], 3);
+            var Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {attribution:'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ', maxZoom: 16 });
+
+            Esri_WorldGrayCanvas.addTo(mymap);
+
+            geojson = L.geoJson(data, {style: style, onEachFeature: onEachFeature}).addTo(mymap);
+})
+
+// var mymap = L.map('mapid').setView([32,-35], 3);
+// var Esri_WorldGrayCanvas = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {attribution:'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ', maxZoom: 16 });
+//
+// Esri_WorldGrayCanvas.addTo(mymap);
+
+mymap.setMinZoom(2);
+
+mymap.setMaxBounds([[90,180], [-90,-179]])
