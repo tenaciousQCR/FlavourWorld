cfdc3b711bc1fdc874d210dc4270091568a5f65a
ZOrvis <a.j.orvis@rgu.ac.uk>
2018-03-22 17:06:18 +0000

    asdfadsf

--- js/mapScript.js
+++ js/mapScript.js
@@ -63,7 +63,7 @@ function onEachFeature(feature, layer) {
 
 
 $.getJSON('../js/custom.geo.json',function(data){
-            mymap = L.map('mapid').setView([32,-35], 3).setMinZoom(2).setMaxBounds([[90,180], [-90,-179]]);
+            mymap = L.map('mapid').setMinZoom(2).setMaxBounds([[90,180], [-90,-179]]).setView([32,-35], 3);
             var Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {attribution:'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ', maxZoom: 16 });
 
             Esri_WorldGrayCanvas.addTo(mymap);
