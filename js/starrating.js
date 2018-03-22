ee1c2fdcfeb8f744730c328f6f9d18a6af98d4cc
Gery Nemeth <ngeri97@gmail.com>
2018-03-22 13:24:28 +0000

    Add files via upload
    this is only the mechanics. star rating is set based on which star object you click on.

---
+++ js/starrating.js
@@ -0,0 +1,39 @@
+$( "star1" ).click(function() {
+  star1 = true;
+  star2 = false;
+  star3 = false;
+  star4 = false;
+  star5 = false;
+});
+
+$( "star2" ).click(function() {
+  star1 = true;
+  star2 = true;
+  star3 = false;
+  star4 = false;
+  star5 = false;
+});
+
+$( "star3" ).click(function() {
+  star1 = true;
+  star2 = true;
+  star3 = true;
+  star4 = false;
+  star5 = false;
+});
+
+$( "star4" ).click(function() {
+  star1 = true;
+  star2 = true;
+  star3 = true;
+  star4 = true;
+  star5 = false;
+});
+
+$( "star5" ).click(function() {
+  star1 = true;
+  star2 = true;
+  star3 = true;
+  star4 = true;
+  star5 = true;
+});
