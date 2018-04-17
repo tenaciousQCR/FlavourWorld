156bdc39821ec081c96344f46964594a43152d64
1600961 <q.c.robertson@rgu.ac.uk>
2018-04-17 15:39:10 +0100

    some db stuff, also im agin' it

--- server.js
+++ server.js
@@ -11,3 +11,41 @@ MongoClient.connect(url, function(err, database){
  db = database;
  app.listen(8080);
 });
+
+app.post('/dologin', function(req, res) {
+  console.log(JSON.stringify(req.body))
+  var uname = req.body.uname;
+  var pword = req.body.psw;
+
+  db.collection('users').findOne({"login.username":uname}, function(err, result) {
+    if (err) throw err;//if there is an error, throw the error
+    //if there is no result, redirect the user back to the login system as that username must not exist
+    if(!result){res.redirect('/login');return}
+    //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the index
+    if(result.login.password == pword){ req.session.loggedin = true; res.redirect('/') }
+    //otherwise send them back to login
+    else{res.redirect('/login')}
+  });
+});
+
+app.post('/registeruser', function(req, res) {
+  //check we are logged in
+  //if(!req.session.loggedin){res.redirect('/login');return;}
+
+  //we create the data string from the form components that have been passed in
+
+var datatostore = {
+"name":{"first":req.body.fname,"last":req.body.lname},
+"email":req.body.email,
+"login":{"username":req.body.street, "password":req.body.password}}
+
+
+
+//once created we just run the data string against the database and all our new data will be saved/
+  db.collection('users').save(datatostore, function(err, result) {
+    if (err) throw err;
+    console.log('saved to database')
+    //when complete redirect to the index
+    res.redirect('/')
+  })
+});
