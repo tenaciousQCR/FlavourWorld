669594ea0838acbf8e725349a09bcec5b7c62f2b
Peter Ramsbottom <PBottom@Peters-Air.lan>
2018-04-18 20:13:26 +0100

    adding some logs to check for problems

--- js/mapScript.js
+++ js/mapScript.js
@@ -40,6 +40,7 @@ function highlightFeature(e) {
 }
 
 function popupFeature(e){
+    console.log("Clicked");
     var targetcountry = e.target.feature.properties.name;
     function getResultsFromYummly(targetcountry){
       var url = "https://api.yummly.com/v1/api/recipes?_app_id=b96a6669&_app_key=68fc92d94c14efafd327d91916587827&q=" + searchterms;
