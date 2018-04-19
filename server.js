3c165692653165285d85c5583ed14c84186d7c53
DAVIDE RENDINA (1610022) <1610022@RG-N525-E02.rgu.ac.uk>
2018-04-19 13:38:09 +0100

    a

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.post('/dologin', function(req, res) {
     if(result.login.password == pword){
       req.session.loggedin = true;
       console.log("you are logged in");
-      res.window.close();
+      $window.close();
     }
 
     //otherwise send them back to login
