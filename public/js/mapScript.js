d3a06fe1bbff3886a3f9fc12fff12c882ffc4e47
Peter Ramsbottom <PBottom@Peters-MacBook-Air.local>
2018-04-19 13:57:27 +0100

    blubelub

--- public/js/mapScript.js
+++ public/js/mapScript.js
@@ -79,13 +79,13 @@ function addResultTitles(jsondata, e){
 
   for (var i = 0; i < length; i++){
     var title = jsondata.matches[i].recipeName;
-    htmlstring += "<button id=\"" + i + "\" onclick=\"getRecipe(this, jsondata)\">" + title + "</button>";
+    htmlstring += "<button id=\"" + i + "\" onclick=\"getRecipe(this)\">" + title + "</button>";
   }
   return htmlstring;
 
 }
 
-function getRecipe(button, jsondata){
+function getRecipe(button){
   console.log(jsondata);
   var selectedRecipe = jsondata.matches[button].id;
   console.log(selectedRecipe);
