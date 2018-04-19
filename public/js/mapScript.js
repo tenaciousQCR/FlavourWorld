95184067528e4be96f23b2f6a164865a38be20e3
Peter Ramsbottom <PBottom@Peters-MacBook-Air.local>
2018-04-19 13:46:40 +0100

    FOOD WARS

--- public/js/mapScript.js
+++ public/js/mapScript.js
@@ -80,6 +80,7 @@ function addResultTitles(jsondata, e){
 
   for (var i = 0; i < length; i++){
     var title = jsondata.matches[i].recipeName;
+    console.log(jsondata);
     htmlstring += "<button id=\"" + i + "\" onclick=\"getRecipe(this, jsondata)\">" + title + "</button>";
   }
   return htmlstring;
