12bd1166d7311a25061db670873191aced764000
Peter Ramsbottom <PBottom@Peters-MacBook-Air.local>
2018-04-26 13:51:59 +0100

    big obi

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get('/', function(req, res) {
 
 // IF THE USER IS NOT LOGGED-IN, THIS REDIRECTS THEM TO THE LOGIN PAGE
 
-  if(!req.session.loggedin){
+  if(req.session.loggedin == null){
     req.session.loggedin = false;
     res.redirect('/home');
     return;
