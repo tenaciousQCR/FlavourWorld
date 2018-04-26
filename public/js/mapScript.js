197d05cfc5258f4c65b0eb9e63ed5c0fdbfdd959
Peter Ramsbottom <PBottom@Peters-Air.lan>
2018-04-26 19:50:17 +0100

    logarithmic largamithmic

--- public/js/mapScript.js
+++ public/js/mapScript.js
@@ -77,6 +77,7 @@ function clickOnMapItem(name){
 
 //Called when a country is clicked, finds the country that is clicked and passes it to the next function
 function popupFeature(e){
+    console.log(e.layer.id);
     var targetcountry = e.target.feature.properties.name;
     getResultsFromYummly(targetcountry, e);
 
