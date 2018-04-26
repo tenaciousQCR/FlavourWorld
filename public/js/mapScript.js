9574fc985e54d4380db9f8c50affcbd019f05016
Peter Ramsbottom <PBottom@Peters-Air.lan>
2018-04-26 20:27:22 +0100

    Right away doctor!

--- public/js/mapScript.js
+++ public/js/mapScript.js
@@ -84,7 +84,7 @@ function clickOnMapItem(name){
 function popupFeature(e){
     console.log(e);
     var targetcountry = e.target.feature.properties.name;
-    console.log(e._bounds)
+    console.log(e.bounds)
     getResultsFromYummly(targetcountry, e);
 
 }
