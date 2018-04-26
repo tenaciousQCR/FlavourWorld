67294e2c9be8542cb17702632f59d5055a066a86
Peter Ramsbottom <PBottom@Peters-Air.lan>
2018-04-26 20:25:14 +0100

    MAAAAPPPYYYYYY

--- public/js/mapScript.js
+++ public/js/mapScript.js
@@ -84,7 +84,7 @@ function clickOnMapItem(name){
 function popupFeature(e){
     console.log(e);
     var targetcountry = e.target.feature.properties.name;
-    console.log(e._latlngs)
+    console.log(e._bounds._northEast)
     getResultsFromYummly(targetcountry, e);
 
 }
