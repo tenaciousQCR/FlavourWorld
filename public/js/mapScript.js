daf0307b0a4bbbea3ebf86c7113a4ad271eb7d52
Peter Ramsbottom <PBottom@Peters-Air.lan>
2018-04-26 19:51:09 +0100

    loggy didnt work

--- public/js/mapScript.js
+++ public/js/mapScript.js
@@ -77,7 +77,7 @@ function clickOnMapItem(name){
 
 //Called when a country is clicked, finds the country that is clicked and passes it to the next function
 function popupFeature(e){
-    console.log(e.layer.id);
+    console.log(e.id);
     var targetcountry = e.target.feature.properties.name;
     getResultsFromYummly(targetcountry, e);
 
